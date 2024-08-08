d6c8e5fdb1657038f7639b15070ba286363e3d76
bagaspra16 <bagaspratamajunianika72@gmail.com>
2024-08-08 09:50:54 +0700

    Update: if inside if

--- SESSION1/main.py
+++ SESSION1/main.py
@@ -1,33 +1,49 @@
 import random
 
-opening = "WELCOME"
-main = "MAIN CONTENT"
-closing = "THANK YOU"
+opening = "WELCOME TO PYTHON'S HIDING GAME"
+closing = "THANK YOU FOR PLAYING!"
 python_position = random.randint(1,4)
 
-print("|-------------------------------------------------------|")
-print(f"|------------------------{opening}------------------------|")
-print("|-------------------------------------------------------|")
+print("|------------------------------------------------------------|")
+print(f"|---------------{opening}--------------|")
+print("|------------------------------------------------------------|")
 print("\n")
 username = input("Input Username : ")
 print("\n")
-print("|-------------------------------------------------------|")
+print("|------------------------------------------------------------|")
 print("\n")
-print(f'''
-        Hello {username}! Come looking at the cave!!!
+print(f'''Hello {username}! Come looking at the cave!!!
               
-              _-1-_  _-2-_  _-3-_  _-4-_
+_-1-_  _-2-_  _-3-_  _-4-_
       ''')
-guess = int(input(f'''
-         In which cave is the python hiding? '''))
+guess = int(input(f'''In which cave is the python hiding? '''))
 print("\n")
-print("|-------------------------------------------------------|")
+confirm = input(f'''Are you sure the python's hiding in {python_position} cave [yes/no]? ''')
 print("\n")
-if guess == python_position:
-    print(f'''You're right {username}!, The python's position is in 
-that number {python_position} cave and you choose that number {guess} cave
-         ''')
+print("|------------------------------------------------------------|")
+print("\n")
+if confirm == "yes":
+    if guess == python_position:
+        print(f'''You're right {username}!, The python's position is in 
+that number {python_position} cave and you choose that number {guess} cave.
+             ''')
+    else:
+        print(f'''You're wrong {username}!, The python's position is in 
+that number {python_position} cave and you choose that number {guess} cave.
+             ''')
 else:
-    print(f'''You're wrong {username}!, The python's position is in 
-that number {python_position} cave and you choose that number {guess} cave
-         ''')
+        guess = int(input(f'''Okay {username}, In which cave is the python hiding? '''))
+        print("\n")
+        print("|------------------------------------------------------------|")
+        print("\n")
+        if guess == python_position:
+            print(f'''You're right {username}!, The python's position is in 
+that number {python_position} cave and you choose that number {guess} cave.
+             ''')
+        else:
+            print(f'''You're wrong {username}!, The python's position is in 
+that number {python_position} cave and you choose that number {guess} cave.
+             ''')
+print("|------------------------------------------------------------|")
+print(f"|-------------------{closing}-------------------|")
+print("|------------------------------------------------------------|")
